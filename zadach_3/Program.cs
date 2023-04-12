f32eead424ee9ea66e77ce44c7e16c4acea7aa25
Luydmila <Ludapanova178@gmail.com>
2023-04-12 14:39:34 +0500

    добавила  3 папки с задачами

---
+++ zadach_3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задайте массив целых чисел. Найдите раздницу между максимальным и минимальным элементом массива  */
+
+Console.WriteLine("Введите количество элементов массива:");
+int n = Convert.ToInt32(Console.ReadLine()); // Задаём размер массива
+int [] mass  = new int[n];      // Создаём массив
+Random rnd = new Random();      // Создание объекта для генерации чисел
+for (int i = 0; i < n; i++)
+    { mass[i] = rnd.Next(-50, 50);  // Заполнение рандомами
+    Console.Write($" {mass[i]} ");
+}
+int min = 0;
+int max = 0;
+foreach (int i in mass)  // Поиск максимального и минимального значения
+{
+    if (min > i) min = i;
+    if (max < i) max = i;
+}
+Console.WriteLine($"\n Максимальный элемент: {max}\n Минимальный элемент: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {max-min}");
